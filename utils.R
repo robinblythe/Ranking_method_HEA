04a68a5c24ba9e83bba04c22cb6197d039d7df81
Robin Blythe <robin.blythe@qut.edu.au>
2024-11-18 22:01:13 +0800

    Playing with number of patients to evaluate

--- utils.R
+++ utils.R
@@ -1,6 +1,6 @@
 obtain_class <- function(df, samples) {
 
-  do.call(rbind, sims) |>
+  do.call(rbind, df) |>
     as_tibble() |>
     mutate(predicted = predict(fit, newdata = pick(x), type = "response"),
            class_val_opt = ifelse(predicted >= cutpoint_nmb, 1, 0),
@@ -11,11 +11,11 @@ obtain_class <- function(df, samples) {
     slice_sample(n = samples) |>
     arrange(desc(predicted)) |>
     mutate(rank = row_number(),
-           classrank5 = ifelse(rank <= 5, 1, 0),
-           classrank10 = ifelse(rank <= 10, 1, 0),
-           classrank15 = ifelse(rank <= 15, 1, 0),
-           classrank20 = ifelse(rank <= 20, 1, 0),
-           classrank25 = ifelse(rank <= 25, 1, 0)
+           classrank3 = ifelse(rank <= 3, 1, 0),
+           classrank6 = ifelse(rank <= 6, 1, 0),
+           classrank9 = ifelse(rank <= 9, 1, 0),
+           classrank12 = ifelse(rank <= 12, 1, 0),
+           classrank15 = ifelse(rank <= 15, 1, 0)
            ) |>
     ungroup() |>
     arrange(auc, p0)
@@ -30,21 +30,21 @@ classifier <- function(classifier, actual, payoffs) {
   )
 }
 
-obtain_payoffs <- function(df) {
-  payoffs <- sampler()
+obtain_payoffs <- function(df, sampler) {
+  payoffs <- sampler
 
   df_payoff <- df |>
     mutate(nmb_val_opt = classifier(class_val_opt, actual, payoffs),
            nmb_nne = classifier(class_nne, actual, payoffs),
            nmb_youden = classifier(class_youden, actual, payoffs),
-           nmb_classrank5 = classifier(classrank5, actual, payoffs),
-           nmb_classrank10 = classifier(classrank10, actual, payoffs),
-           nmb_classrank15 = classifier(classrank15, actual, payoffs),
-           nmb_classrank20 = classifier(classrank20, actual, payoffs),
-           nmb_classrank25 = classifier(classrank25, actual, payoffs)
+           nmb_classrank3 = classifier(classrank3, actual, payoffs),
+           nmb_classrank6 = classifier(classrank6, actual, payoffs),
+           nmb_classrank9 = classifier(classrank9, actual, payoffs),
+           nmb_classrank12 = classifier(classrank12, actual, payoffs),
+           nmb_classrank15 = classifier(classrank15, actual, payoffs)
            ) |>
     group_by(auc, p0) |>
-    summarise(across(nmb_val_opt:nmb_classrank25, sum))
+    summarise(across(nmb_val_opt:nmb_classrank15, mean))
   
   return(df_payoff)
 }
