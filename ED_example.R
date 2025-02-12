bdb9c566b4a23df7608efd599c8d4af837d09c4a
Robin Blythe <robin.blythe@qut.edu.au>
2025-02-12 11:16:55 +0800

    Added scenario analysis

--- ED_example.R
+++ ED_example.R
@@ -3,12 +3,14 @@ options(scipen = 100, digits = 5)
 library(tidyverse)
 
 # Import data
-df_ed_raw <- readRDS("C:/Users/blythe/NUS Dropbox/EDData/ED2A/ED2A_Blythe Robin Daniel/data_all_temp2022.11.18.RDS")
+df_ed_raw <- readRDS("C:/Users/Robin/NUS Dropbox/EDData/ED2A/ED2A_Blythe Robin Daniel/data_all_temp2022.11.18.RDS")
+
 df_ed <- df_ed_raw |>
   filter(REGISTRATION_DATE >= "2017-01-01") |>
   mutate(ID = row_number(),
-         outcome = ifelse(outcome_mortality_2d == TRUE, 1, 0)) |>
-  select(ID, AGE, PULSE, RESPIRATION, BPSYSTOLIC, BPDIASTOLIC, AllCancer, outcome) |>
+         outcome_icu = ifelse(n_icu_overall_current > 0, 1, 0),
+         outcome_died_30d = ifelse(outcome_mortality_30d == TRUE, 1, 0)) |>
+  select(ID, AGE, PULSE, RESPIRATION, BPSYSTOLIC, BPDIASTOLIC, AllCancer, outcome_admit, outcome_icu, outcome_died_30d) |>
   rowwise() |>
   mutate(pred_risk = sum(
     case_when(AGE < 30 ~ 0,
@@ -34,7 +36,8 @@ df_ed <- df_ed_raw |>
               AllCancer == "1local" ~ 6,
               AllCancer == "2metastatic" ~ 14)
     )) |>
+  ungroup() |>
   na.omit()
 
-saveRDS(df_ed, file = "C:/Users/blythe/NUS Dropbox/EDData/ED2A/ED2A_Blythe Robin Daniel/dat_scored.RDS")
+saveRDS(df_ed, file = "C:/Users/Robin/NUS Dropbox/EDData/ED2A/ED2A_Blythe Robin Daniel/dat_scored.RDS")
 remove(df_ed_raw)
